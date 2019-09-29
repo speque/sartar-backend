82b933ca443b3f8f8d3344e90fa6868d26153c11
Pekka Salmela <pekka.salmela@gmail.com>
2019-09-29 12:41:10 +0300

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/speque/sartar-backend"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/spec.alpha "0.2.176"]
                  [compojure "1.6.1"]
                  [ring-cors "0.1.13"]
                  [ring-logger "1.0.1"]
-                 [ring/ring-json "0.4.0"]
+                 [ring/ring-json "0.5.0"]
                  [orchestra "2019.02.06-1"]
-                 [com.walmartlabs/lacinia "0.32.0"]]
+                 [com.walmartlabs/lacinia "0.35.0"]]
   :target-path "target/%s"
   :profiles {
-              :dev {:dependencies [[metosin/testit "0.3.0"]
-                                   [ring/ring-mock "0.3.2"]]}
+              :dev {:dependencies [[metosin/testit "0.4.0"]
+                                   [ring/ring-mock "0.4.0"]]}
               :uberjar {:aot :all}
-              :kaocha {:dependencies [[lambdaisland/kaocha "0.0-389"]]}
+              :kaocha {:dependencies [[lambdaisland/kaocha "0.0-541"]]}
             }
   :plugins [[lein-ring "0.9.7"]
             [lein-cljfmt "0.6.0"]]
